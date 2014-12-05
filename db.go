6379f292a1faa5fb5400de5ebd16c5618e7f1d88
tpbstats <ayeytvhh@sharklasers.com>
2014-12-05 18:51:23 +0100

    Changed away from stupid Heroku config

--- db.go
+++ db.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"os"
-	"fmt"
-	"regexp"
 	"database/sql"
 	_ "github.com/lib/pq"
 )
 
 func getDb() *sql.DB {
-
-	// https://groups.google.com/d/msg/golang-nuts/0XNrQRFuvoc/qDH_vXqN8j0J
-	// postgres://user:password@host:port/dbname
-	rex := regexp.MustCompile("(?i)^postgres://(?:([^:@]+):([^@]*)@)?([^@/:]+):(\\d+)/(.*)$")
-	matches := rex.FindStringSubmatch(os.Getenv("DATABASE_URL"))
-	spec := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", matches[1], matches[2], matches[3], matches[4], matches[5])
-
-	db, err := sql.Open("postgres", spec)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE"))
 	if err != nil {
 		panic(err)
 	}
